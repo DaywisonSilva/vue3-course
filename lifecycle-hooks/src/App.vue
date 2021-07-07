e7390d02ac22fc59ee103da967c5793403f44d25
Daywison Silva <daywisonsilva@gmail.com>
2021-07-07 13:47:22 -0300

    conhecendo o projeto

---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Lifecycle Hooks</h1>
+
+  </div>
+</template>
+
+<script>
+import { onMounted } from '@vue/runtime-core'
+export default {
+  setup() {
+    onMounted(() => {
+      console.log("mo ntado com onMounted")
+    })
+  }
+}
+
+
+// export default {
+//   mounted() {
+//     console.log('Foi montado options API')
+//   }
+// }
+// </script>
